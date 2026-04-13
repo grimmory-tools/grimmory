fc0ee70d394a39ba39c910a706b1e56e056ad8dd
Balázs Szücs <bszucs1209@gmail.com>
2026-04-13 17:58:30 +0200

    fix(metadata): refactor book metadata fetching to use fetch API and signals for state management

--- booklore-api/src/main/java/org/booklore/service/metadata/parser/BookParser.java
+++ booklore-api/src/main/java/org/booklore/service/metadata/parser/BookParser.java
@@ -3,12 +3,17 @@ package org.booklore.service.metadata.parser;
 import org.booklore.model.dto.Book;
 import org.booklore.model.dto.BookMetadata;
 import org.booklore.model.dto.request.FetchMetadataRequest;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 public interface BookParser {
 
     List<BookMetadata> fetchMetadata(Book book, FetchMetadataRequest fetchMetadataRequest);
+
+    default Flux<BookMetadata> fetchMetadataStream(Book book, FetchMetadataRequest fetchMetadataRequest) {
+        return Flux.fromIterable(fetchMetadata(book, fetchMetadataRequest));
+    }
 
     BookMetadata fetchTopMetadata(Book book, FetchMetadataRequest fetchMetadataRequest);
 }
