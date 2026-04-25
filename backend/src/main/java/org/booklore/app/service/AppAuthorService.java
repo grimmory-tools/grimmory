fb47eb07ea369b6c26e6df413ac2f863a163f1a7
Balázs Szücs <bszucs1209@gmail.com>
2026-04-25 23:15:32 +0200

    fix(api): add missing import

--- backend/src/main/java/org/booklore/app/service/AppAuthorService.java
+++ backend/src/main/java/org/booklore/app/service/AppAuthorService.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
