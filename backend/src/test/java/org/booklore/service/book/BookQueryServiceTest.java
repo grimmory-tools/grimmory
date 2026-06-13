d3530b62d31b567210996ffb0d42ae92e969f14c
Alex <25013571+alexhb1@users.noreply.github.com>
2026-06-13 13:36:10 +0100

    fix(shelf): update test

--- backend/src/test/java/org/booklore/service/book/BookQueryServiceTest.java
+++ backend/src/test/java/org/booklore/service/book/BookQueryServiceTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,6 +29,7 @@ class BookQueryServiceTest {
 
         List<Book> result = bookQueryService.mapEntitiesToDto(List.of(bookEntity), true, 1L);
 
-        assertEquals(Set.of(ownShelf, otherPublicShelf), result.getFirst().getShelves());
+        assertThat(result.getFirst().getShelves())
+                .containsExactlyInAnyOrder(ownShelf, otherPublicShelf);
     }
 }
