86d9ff2f406bd8dc5ee2b0f33720ef0fe9b018f9
Alex <25013571+alexhb1@users.noreply.github.com>
2026-07-03 09:45:46 +0100

    fix(migration): update migration to use temp table

--- backend/src/main/resources/db/migration/V145__Migrate_bundled_custom_svg_icons_to_lucide.sql
+++ backend/src/main/resources/db/migration/V145__Migrate_bundled_custom_svg_icons_to_lucide.sql
@@ -1,29 +1,49 @@
+SET @custom_svg_icon_type = 'CUSTOM_SVG';
+SET @lucide_icon_type = 'LUCIDE';
+
+CREATE TEMPORARY TABLE bundled_custom_svg_icon AS
+SELECT icon AS icon_name
+FROM library
+WHERE 1 = 0;
+
+INSERT INTO bundled_custom_svg_icon (icon_name) VALUES
+    ('atom'),
+    ('banana'),
+    ('beef'),
+    ('brain'),
+    ('chef-hat'),
+    ('drama'),
+    ('ferris-wheel'),
+    ('flame-kindling'),
+    ('ghost'),
+    ('hamburger'),
+    ('plane'),
+    ('rocket'),
+    ('roller-coaster'),
+    ('rose'),
+    ('skull'),
+    ('snail'),
+    ('swords'),
+    ('tent-tree'),
+    ('tree-palm'),
+    ('turntable');
+
 UPDATE library
-SET icon_type = 'LUCIDE'
-WHERE icon_type = 'CUSTOM_SVG'
-  AND icon IN (
-    'atom', 'banana', 'beef', 'brain', 'chef-hat',
-    'drama', 'ferris-wheel', 'flame-kindling', 'ghost', 'hamburger',
-    'plane', 'rocket', 'roller-coaster', 'rose', 'skull',
-    'snail', 'swords', 'tent-tree', 'tree-palm', 'turntable'
-  );
+INNER JOIN bundled_custom_svg_icon
+  ON library.icon = bundled_custom_svg_icon.icon_name
+SET library.icon_type = @lucide_icon_type
+WHERE library.icon_type = @custom_svg_icon_type;
 
 UPDATE shelf
-SET icon_type = 'LUCIDE'
-WHERE icon_type = 'CUSTOM_SVG'
-  AND icon IN (
-    'atom', 'banana', 'beef', 'brain', 'chef-hat',
-    'drama', 'ferris-wheel', 'flame-kindling', 'ghost', 'hamburger',
-    'plane', 'rocket', 'roller-coaster', 'rose', 'skull',
-    'snail', 'swords', 'tent-tree', 'tree-palm', 'turntable'
-  );
+INNER JOIN bundled_custom_svg_icon
+  ON shelf.icon = bundled_custom_svg_icon.icon_name
+SET shelf.icon_type = @lucide_icon_type
+WHERE shelf.icon_type = @custom_svg_icon_type;
 
 UPDATE magic_shelf
-SET icon_type = 'LUCIDE'
-WHERE icon_type = 'CUSTOM_SVG'
-  AND icon IN (
-    'atom', 'banana', 'beef', 'brain', 'chef-hat',
-    'drama', 'ferris-wheel', 'flame-kindling', 'ghost', 'hamburger',
-    'plane', 'rocket', 'roller-coaster', 'rose', 'skull',
-    'snail', 'swords', 'tent-tree', 'tree-palm', 'turntable'
-  );
+INNER JOIN bundled_custom_svg_icon
+  ON magic_shelf.icon = bundled_custom_svg_icon.icon_name
+SET magic_shelf.icon_type = @lucide_icon_type
+WHERE magic_shelf.icon_type = @custom_svg_icon_type;
+
+DROP TEMPORARY TABLE bundled_custom_svg_icon;
